1caf26e33f3a17824fb5c7a64fe86eba3304ad74
esteban cardozo <esteban.cardozo.wec@gmail.com>
2025-05-12 12:50:45 -0300

    clase 5 antes de las vistas

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ import { Slot, Stack } from "expo-router";
 const AuthLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} /> 
+      <Stack.Screen name="index" options={{ headerShown: true }} /> 
     </Stack>
   );
 };
--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Auth = () => {
   return (
-    <View>
-      <Text>Pantalla de autenticación</Text>
-    </View>
-  )
-}
-
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text>Auth</Text>
+      </View>
+      <View style={styles.body}>
+        <Image></Image>
+      </View>
+      <View style={styles.footer}>
+        <Text>Auth</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 70,
+  },
+  header: {
+    // height: 40,
+    // width: 40,
+    // backgroundColor: "red",
+  },
+  body: {
+    // backgroundColor: "blue",
+    // height: 40,
+    // width: 40,
+  },
+  footer: {
+    // backgroundColor: "green",
+    // height: 40,
+    // width: 40,
+  },
+});
 export default Auth;
