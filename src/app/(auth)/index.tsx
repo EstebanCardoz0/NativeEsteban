8a49360b9182b962cc0a9005f5dc3d4161de68bb
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2025-05-11 21:27:17 -0300

    clase 4 completa layouts

---
+++ src/app/(auth)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <View>
+      <Text>Pantalla de autenticación</Text>
+    </View>
+  )
+}
+
+export default Auth;
---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Redirect, Stack } from "expo-router";
+import React from "react";
+import { View, Text } from "react-native";
+
+const RootNavigation = () => {
+  const [isLogin, setIsLogin] = React.useState(true);
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}></Stack>
+      {isLogin ? <Redirect href="/(main)" /> : <Redirect href="/(auth)" />}
+    </>
+  );
+};
+
+export default RootNavigation;
